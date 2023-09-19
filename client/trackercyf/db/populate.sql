ce7025466226ec57b7386e652d28270f3bd1774d
softacoder <113185323+softacoder@users.noreply.github.com>
2023-09-19 09:50:32 +0100

    commit update server and sql

---
+++ client/trackercyf/db/populate.sql
@@ -0,0 +1,14 @@
+-- version1. this version is from Annas first update version.
+DROP TABLE IF EXISTS task;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS to_do;
+CREATE TABLE task (id SERIAL PRIMARY KEY,
+                                          module VARCHAR(50) NOT NULL,
+                                                           name VARCHAR(100),
+                                                                   due_date DATE);
+                                                                        
+CREATE TABLE users (id SERIAL PRIMARY KEY,
+                                          name VARCHAR(100) NOT NULL );
+                                          
+CREATE TABLE to_do (id SERIAL PRIMARY KEY, task_id INT NOT NULL REFERENCES task(id), user_id INT NOT NULL REFERENCES users(id), done BOOLEAN not NULL,
+                                        feedback TEXT, FOREIGN KEY (task_id,user_id) REFERENCES task (id) REFERENCES users (id));
